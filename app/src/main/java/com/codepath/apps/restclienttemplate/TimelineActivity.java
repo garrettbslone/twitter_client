fb3bfc4514646a591654475551284d292ce25a57
Garrett Slone <gsloneca@gmail.com>
2021-03-12 14:20:50 -0800

    update menu styling to have twitter theme and compose button

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,15 +1,19 @@
 package com.codepath.apps.restclienttemplate;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Adapter;
 import android.widget.Toast;
 
@@ -20,6 +24,7 @@ import com.facebook.stetho.inspector.jsonrpc.JsonRpcException;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,29 +34,22 @@ import okhttp3.Headers;
 
 public class TimelineActivity extends AppCompatActivity {
     public static final String TAG = "TimelineActivity";
+    private final int REQUEST_CODE = 20;
 
     TwitterClient client;
-
 
     List<Tweet> tweets;
     TweetsAdapter adapter;
 
     RecyclerView rvTweets;
     SwipeRefreshLayout swipeContainer;
-    
+
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
 
-        ActionBar bar = getSupportActionBar();
-        assert bar != null;
-        bar.setDisplayShowTitleEnabled(false);
-        ColorDrawable colorDrawable
-                = new ColorDrawable(Color.parseColor("#00ACEE"));
-        bar.setBackgroundDrawable(colorDrawable);
-        bar.setLogo(R.drawable.ic_logo_white);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        setupActionBar();
 
         client = TwitterApp.getRestClient(this);
 
@@ -79,6 +77,35 @@ public class TimelineActivity extends AppCompatActivity {
         populateHomeTimeline();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        if (item.getItemId() == R.id.compose) {
+            Intent intent = new Intent(this, ComposeActivity.class);
+            startActivityForResult(intent, REQUEST_CODE);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            Tweet tweet = Parcels.unwrap(data.getParcelableExtra("tweet"));
+            tweets.add(0, tweet);
+            adapter.notifyItemInserted(0);
+            rvTweets.smoothScrollToPosition(0);
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
     private void populateHomeTimeline () {
         client.getHomeTimeline(new JsonHttpResponseHandler() {
             @Override
@@ -100,5 +127,17 @@ public class TimelineActivity extends AppCompatActivity {
                 Log.e(TAG, "onFailure! " + response, throwable);
             }
         });
+    }
+
+    private void setupActionBar() {
+        ActionBar bar = getSupportActionBar();
+        assert bar != null;
+        bar.setDisplayShowTitleEnabled(false);
+        ColorDrawable colorDrawable
+                = new ColorDrawable(Color.parseColor("#00ACEE"));
+        bar.setBackgroundDrawable(colorDrawable);
+        bar.setDisplayShowHomeEnabled(true);
+        bar.setLogo(R.drawable.ic_logo_white);
+        bar.setDisplayUseLogoEnabled(true);
     }
 }
